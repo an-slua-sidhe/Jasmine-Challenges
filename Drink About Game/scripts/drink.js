10c80cc7975b2b42bd1cfef003f09c9632cd931c
Paul Quinn <polocoinn@yahoo.ie>
2019-11-28 17:15:40 +0000

    Drink About Game files completed

--- Drink About Game/scripts/drink.js
+++ Drink About Game/scripts/drink.js
@@ -1,19 +1,19 @@
 function whatCanIDrink(age){
-    if{
-        age < 0 = return "Sorry. I can't tell what drink to have because that age you aren't even born yet!"
+    if(age < 0){
+        return "Sorry. I can't tell you what drink to have because that age you aren't even born yet!"
     }
-    if{
-        age < 14 = return "Drink Tanora";
+    if(age < 14){
+        return "Drink Tanora";
     }
-    if{
-        age < 18 = return "Drink Coke";
+    if(age < 18 && age >= 14){
+        return "Drink Coke";
     }
-    if{
-        age < 21 = return "Drink Beer";
+    if(age < 21 && age >= 18){
+        return "Drink Beer";
     }
-    if{
-        age < 130 = return "Drink Hist Sap";
+    if(age < 130 && age >= 21){
+        return "Drink Hist Sap";
     } else {
-        return "Sorry. I can't tell what drink to have because that age doesn't make sense! :-)"
+        return "Sorry. I can't tell you what drink to have because that age doesn't make sense! :-)"
     }
 }
